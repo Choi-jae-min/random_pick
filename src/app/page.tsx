2420d9fc92a077dd7871433bbd2dad5a221235c3
jaeminSmartel <129919643+jaeminSmartel@users.noreply.github.com>
2025-02-25 15:02:39 +0900

    feat : 메인 페이지 해더

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto font-[family-name:var(--font-geist-sans)]">
+        <header className={'py-10 space-x-10 flex justify-center border-b '}>
+            <Link className={'hover:text-gray-500 transition-colors'} href={''}>랜덤 뽑기</Link>
+            <Link className={'hover:text-gray-500 transition-colors'} href={''}>사다리 타기</Link>
+            <Link className={'hover:text-gray-500 transition-colors'} href={'/team'}>랜덤 팀/조 편성</Link>
+            <Link className={'hover:text-gray-500 transition-colors'} href={''}>랜덤 번호 생성기</Link>
+            <Link className={'hover:text-gray-500 transition-colors'} href={''}>가위 바위 보 랜덤 승부</Link>
+            <Link className={'hover:text-gray-500 transition-colors'} href={''}>룰렛 만들기</Link>
+        </header>
       <main>
+
       </main>
     </div>
   );
